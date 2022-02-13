4cd3e972a0ca0e198794478963ae278630b1fd40
Kyllox <kyllox4804@gmail.com>
2022-02-13 11:00:52 +0900

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -124,224 +124,223 @@
         $(document).on('click', '.mfp-wrap', function(e) {
             scrollAble();
         });
-	});
 
-	$('body').prepend('<a href="#top" class="back-to-top page-scroll"></a>');
-    var amountScrolled = 450;
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > amountScrolled) {
-            $('a.back-to-top').fadeIn('500');
-        } else {
-            $('a.back-to-top').fadeOut('500');
-        }
-    });
+        $('body').prepend('<a href="#top" class="back-to-top page-scroll"></a>');
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > 500) {
+                $('a.back-to-top').fadeIn();
+            } else {
+                $('a.back-to-top').fadeOut();
+            }
+        });
 
-    $(".ham").click(function(){
-        $(this).toggleClass("active");
-        $('.slide-menu-container').slideToggle();
-    });
+        $(".ham").click(function(){
+            $(this).toggleClass("active");
+            $('.slide-menu-container').slideToggle();
+        });
 
-    $(window).resize(function() {
-        var windowWidth = $(window).width();
+        $(window).resize(function() {
+            var windowWidth = $(window).width();
 
-        if (windowWidth > 768) {
-            $('.slide-menu-container').hide();
+            if (windowWidth > 768) {
+                $('.slide-menu-container').hide();
+                $('.ham').removeClass("active");
+            }
+        });
+
+        $('#slide-menu .page-scroll').click(function() {
             $('.ham').removeClass("active");
-        }
-    });
-
-    $('#slide-menu .page-scroll').click(function() {
-        $('.ham').removeClass("active");
-        $('.slide-menu-container').slideUp('fast');
-    });
-
-    $('#pages-down').click(function() {
-        $('#pages-down').hide();
-        $('#pages-up').show();
-        $('#slide-menu-pages').slideDown('fast');
-        $('#slide-pages-btn').toggleClass("active");
-        $('#pages-up').toggleClass("active");
-    });
-
-    $('#pages-up').click(function() {
-        $('#pages-down').show();
-        $('#pages-up').hide();
-        $('#slide-menu-pages').slideUp('fast');
-        $('#slide-pages-btn').toggleClass("active");
-        $('#pages-up').toggleClass("active");
-    });
-
-    $('#slide-pages-btn').click(function() {
-        $('#pages-down').toggle();
-        $('#pages-up').toggle();
-        $('#slide-menu-pages').slideToggle('fast');
-        $('#slide-pages-btn').toggleClass("active");
-        $('#pages-up').toggleClass("active");
-    });
-
-    $('#lang-down').click(function() {
-        $('#lang-down').hide();
-        $('#lang-up').show();
-        $('#slide-menu-lang').slideDown('fast');
-        $('#slide-lang-btn').toggleClass("active");
-        $('#lang-up').toggleClass("active");
-    });
-
-    $('#lang-up').click(function() {
-        $('#lang-down').show();
-        $('#lang-up').hide();
-        $('#slide-menu-lang').slideUp('fast');
-        $('#slide-lang-btn').toggleClass("active");
-        $('#lang-up').toggleClass("active");
-    });
-
-    $('#slide-lang-btn').click(function() {
-        $('#lang-down').toggle();
-        $('#lang-up').toggle();
-        $('#slide-menu-lang').slideToggle('fast');
-        $('#slide-lang-btn').toggleClass("active");
-        $('#lang-up').toggleClass("active");
-    });
-
-    $('#donate-ko').click(function() {
-        var win = window.open('https://toss.me/kyllox/5000', '_blank noopener noreferrer');
-        win.focus();
-    });
-
-    $('#donate-en').click(function() {
-        var win = window.open('https://paypal.me/kyllox4804', '_blank noopener noreferrer');
-        win.focus();
-    });
-
-    const contactFormKo = function() {
-        $('#contact-form-ko').validate({
-            rules: {
-                "name-ko": {
-                    required: true,
-                    maxlength: 35
-                },
-                "title-ko": {
-                    required: true,
-                    maxlength: 35
-                },
-                "email-ko": {
-                    required: true,
-                    email: true
-                },
-                "message-ko": {
-                    required: true,
-                    minlength: 5,
-                    maxlength: 500
-                }
-            },
-            messages: {
-                "name-ko": {
-                    required: "이름을 입력해주세요.",
-                    maxlength: "이름은 최대 35글자까지 입력 가능합니다."
-                },
-                "title-ko": {
-                    required: "제목을 입력해주세요.",
-                    maxlength: "제목은 최대 35글자까지 입력 가능합니다."
-                },
-                "email-ko": {
-                    required: "이메일 주소를 입력해주세요.",
-                    email: "올바른 이메일 주소를 입력해주세요."
-                },
-                "message-ko": {
-                    required: "메세지를 입력해주세요.",
-                    minlength: "메세지를 최소 5글자 이상 입력해주세요.",
-                    maxlength: "메세지는 최대 500글자까지 입력 가능합니다."
-                }
-            },
-            submitHandler: function() {
-                if ($('#g-recaptcha-response').val() == "") {
-                    $('#error-label').html("<label id='recaptcha-error' class='error' for='#'>reCAPTCHA 인증을 진행해주세요.</label>");
-                } else {
-                    const template = {	
-                        name: $('input[name=name-ko]').val(),
-                        title: $('input[name=title-ko]').val(),
-                        email : $('input[name=email-ko]').val(),
-                        message : $('textarea[name=message-ko]').val()
-                    };
-            
-                    emailjs.send('service_kyllox', 'template_kyllox', template).then(function(response) {
-                        window.alert("이메일을 성공적으로 전송했습니다. 작성해주신 이메일 주소로 답변이 전송됩니다." + "\n" + response.status + " " + response.text);
-                        $('input[name=name-ko]').val("");
-                        $('input[name=title-ko]').val("");
-                        $('input[name=email-ko]').val("");
-                        $('textarea[name=message-ko]').val("");
-                    }, function(e) {
-                        window.alert("이메일 전송에 실패하였습니다. 잠시 후 다시 시도해주세요." + "\n" + e);
-                    });
-                }
-            }
+            $('.slide-menu-container').slideUp('fast');
         });
-    };
 
-    const contactFormEn = function() {
-        $('#contact-form-en').validate({
-            rules: {
-                "name-en": {
-                    required: true,
-                    maxlength: 35
-                },
-                "title-en": {
-                    required: true,
-                    maxlength: 35
-                },
-                "email-en": {
-                    required: true,
-                    email: true
-                },
-                "message-en": {
-                    required: true,
-                    minlength: 5,
-                    maxlength: 500
-                }
-            },
-            messages: {
-                "name-en": {
-                    required: "Please enter your name.",
-                    maxlength: "The name can be enter up to 35 characters long."
-                },
-                "title-en": {
-                    required: "Please enter a title",
-                    maxlength: "The title can be enter up to 35 characters long."
-                },
-                "email-en": {
-                    required: "Please enter a email address.",
-                    email: "Please enter a valid email address."
-                },
-                "message-en": {
-                    required: "Please enter a message.",
-                    minlength: "Please enter at least 5 letters of message.",
-                    maxlength: "The message can be enter up to 500 characters long."
-                },
-            },
-            submitHandler: function() {
-                if ($('#g-recaptcha-response').val() == "") {
-                    $('#error-label').html("<label id='recaptcha-error' class='error' for='#'>Please proceed with the reCAPTCHA certification.</label>");
-                } else {
-                    const template = {	
-                        name: $('input[name=name-en]').val(),
-                        title: $('input[name=title-en]').val(),
-                        email : $('input[name=email-en]').val(),
-                        message : $('textarea[name=message-en]').val()
-                    };
-            
-                    emailjs.send('service_kyllox', 'template_kyllox', template).then(function(response) {
-                        window.alert("You have successfully sent an email. I will send my answer to your email address." + "\n" + response.status + " " + response.text);
-                        $('input[name=name-en]').val("");
-                        $('input[name=title-en]').val("");
-                        $('input[name=email-en]').val("");
-                        $('textarea[name=message-en]').val("");
-                    }, function(e) {
-                        window.alert("Failed to send email. Please try again in a few minutes." + "\n" + e);
-                    });
-                }
-            }
+        $('#pages-down').click(function() {
+            $('#pages-down').hide();
+            $('#pages-up').show();
+            $('#slide-menu-pages').slideDown('fast');
+            $('#slide-pages-btn').toggleClass("active");
+            $('#pages-up').toggleClass("active");
         });
-    };
+
+        $('#pages-up').click(function() {
+            $('#pages-down').show();
+            $('#pages-up').hide();
+            $('#slide-menu-pages').slideUp('fast');
+            $('#slide-pages-btn').toggleClass("active");
+            $('#pages-up').toggleClass("active");
+        });
+
+        $('#slide-pages-btn').click(function() {
+            $('#pages-down').toggle();
+            $('#pages-up').toggle();
+            $('#slide-menu-pages').slideToggle('fast');
+            $('#slide-pages-btn').toggleClass("active");
+            $('#pages-up').toggleClass("active");
+        });
+
+        $('#lang-down').click(function() {
+            $('#lang-down').hide();
+            $('#lang-up').show();
+            $('#slide-menu-lang').slideDown('fast');
+            $('#slide-lang-btn').toggleClass("active");
+            $('#lang-up').toggleClass("active");
+        });
+
+        $('#lang-up').click(function() {
+            $('#lang-down').show();
+            $('#lang-up').hide();
+            $('#slide-menu-lang').slideUp('fast');
+            $('#slide-lang-btn').toggleClass("active");
+            $('#lang-up').toggleClass("active");
+        });
+
+        $('#slide-lang-btn').click(function() {
+            $('#lang-down').toggle();
+            $('#lang-up').toggle();
+            $('#slide-menu-lang').slideToggle('fast');
+            $('#slide-lang-btn').toggleClass("active");
+            $('#lang-up').toggleClass("active");
+        });
+
+        $('#donate-ko').click(function() {
+            var win = window.open('https://toss.me/kyllox/5000', '_blank noopener noreferrer');
+            win.focus();
+        });
+
+        $('#donate-en').click(function() {
+            var win = window.open('https://paypal.me/kyllox4804', '_blank noopener noreferrer');
+            win.focus();
+        });
+
+        const contactFormKo = function() {
+            $('#contact-form-ko').validate({
+                rules: {
+                    "name-ko": {
+                        required: true,
+                        maxlength: 35
+                    },
+                    "title-ko": {
+                        required: true,
+                        maxlength: 35
+                    },
+                    "email-ko": {
+                        required: true,
+                        email: true
+                    },
+                    "message-ko": {
+                        required: true,
+                        minlength: 5,
+                        maxlength: 500
+                    }
+                },
+                messages: {
+                    "name-ko": {
+                        required: "이름을 입력해주세요.",
+                        maxlength: "이름은 최대 35글자까지 입력 가능합니다."
+                    },
+                    "title-ko": {
+                        required: "제목을 입력해주세요.",
+                        maxlength: "제목은 최대 35글자까지 입력 가능합니다."
+                    },
+                    "email-ko": {
+                        required: "이메일 주소를 입력해주세요.",
+                        email: "올바른 이메일 주소를 입력해주세요."
+                    },
+                    "message-ko": {
+                        required: "메세지를 입력해주세요.",
+                        minlength: "메세지를 최소 5글자 이상 입력해주세요.",
+                        maxlength: "메세지는 최대 500글자까지 입력 가능합니다."
+                    }
+                },
+                submitHandler: function() {
+                    if ($('#g-recaptcha-response').val() == "") {
+                        $('#error-label').html("<label id='recaptcha-error' class='error' for='#'>reCAPTCHA 인증을 진행해주세요.</label>");
+                    } else {
+                        const template = {	
+                            name: $('input[name=name-ko]').val(),
+                            title: $('input[name=title-ko]').val(),
+                            email : $('input[name=email-ko]').val(),
+                            message : $('textarea[name=message-ko]').val()
+                        };
+                
+                        emailjs.send('service_kyllox', 'template_kyllox', template).then(function(response) {
+                            window.alert("이메일을 성공적으로 전송했습니다. 작성해주신 이메일 주소로 답변이 전송됩니다." + "\n" + response.status + " " + response.text);
+                            $('input[name=name-ko]').val("");
+                            $('input[name=title-ko]').val("");
+                            $('input[name=email-ko]').val("");
+                            $('textarea[name=message-ko]').val("");
+                        }, function(e) {
+                            window.alert("이메일 전송에 실패하였습니다. 잠시 후 다시 시도해주세요." + "\n" + e);
+                        });
+                    }
+                }
+            });
+        };
+
+        const contactFormEn = function() {
+            $('#contact-form-en').validate({
+                rules: {
+                    "name-en": {
+                        required: true,
+                        maxlength: 35
+                    },
+                    "title-en": {
+                        required: true,
+                        maxlength: 35
+                    },
+                    "email-en": {
+                        required: true,
+                        email: true
+                    },
+                    "message-en": {
+                        required: true,
+                        minlength: 5,
+                        maxlength: 500
+                    }
+                },
+                messages: {
+                    "name-en": {
+                        required: "Please enter your name.",
+                        maxlength: "The name can be enter up to 35 characters long."
+                    },
+                    "title-en": {
+                        required: "Please enter a title",
+                        maxlength: "The title can be enter up to 35 characters long."
+                    },
+                    "email-en": {
+                        required: "Please enter a email address.",
+                        email: "Please enter a valid email address."
+                    },
+                    "message-en": {
+                        required: "Please enter a message.",
+                        minlength: "Please enter at least 5 letters of message.",
+                        maxlength: "The message can be enter up to 500 characters long."
+                    },
+                },
+                submitHandler: function() {
+                    if ($('#g-recaptcha-response').val() == "") {
+                        $('#error-label').html("<label id='recaptcha-error' class='error' for='#'>Please proceed with the reCAPTCHA certification.</label>");
+                    } else {
+                        const template = {	
+                            name: $('input[name=name-en]').val(),
+                            title: $('input[name=title-en]').val(),
+                            email : $('input[name=email-en]').val(),
+                            message : $('textarea[name=message-en]').val()
+                        };
+                
+                        emailjs.send('service_kyllox', 'template_kyllox', template).then(function(response) {
+                            window.alert("You have successfully sent an email. I will send my answer to your email address." + "\n" + response.status + " " + response.text);
+                            $('input[name=name-en]').val("");
+                            $('input[name=title-en]').val("");
+                            $('input[name=email-en]').val("");
+                            $('textarea[name=message-en]').val("");
+                        }, function(e) {
+                            window.alert("Failed to send email. Please try again in a few minutes." + "\n" + e);
+                        });
+                    }
+                }
+            });
+        };
+	});
 })(jQuery);
 
 const slide = new Swiper('.slide-content', {
