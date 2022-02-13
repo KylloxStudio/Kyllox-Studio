97c86a35e5b272133b63849e164866aa793be6e2
Kyllox <kyllox4804@gmail.com>
2022-02-14 07:14:27 +0900

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -76,6 +76,7 @@
                         $('#contact-form-ko').fadeOut();
                         $('#message-success').html("<i class='fa fa-check'></i>이메일을 성공적으로 전송했습니다. 작성해주신 이메일 주소로 답변이 전송됩니다.</p><br>" + "<p>" + response.status + "</p>" + " " + response.text);
                         $('#message-success').fadeIn();
+                        $('#message-warning').hide();
                     }, function(e) {
                         loader.fadeOut();
                         $('#message-warning').html("<i class='fa fa-xmark'></i>이메일 전송에 실패하였습니다. 잠시 후 다시 시도해주세요.<br>" + "<p>" + e.status + "</p>" + " " + e.text);
@@ -146,6 +147,7 @@
                         $('#contact-form-en').fadeOut();
                         $('#message-success').html("<i class='fa fa-check'></i>You have successfully sent an email. I will send my answer to your email address.<br>" + "<p>" + response.status + "</p>" + " " + response.text);
                         $('#message-success').fadeIn();
+                        $('#message-warning').hide();
                     }, function(e) {
                         window.alert("Failed to send email. Please try again in a few minutes." + "\n" + e.status + " " + e.text);
                         $('#message-warning').html("<i class='fa fa-xmark'></i>Failed to send email. Please try again in a few minutes.<br>" + "<p>" + e.status + "<p>" + " " + e.text);
